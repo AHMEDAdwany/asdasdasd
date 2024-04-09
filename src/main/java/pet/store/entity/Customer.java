214da0934c14cdae3364d72e6e786c0c085f1403
AHMEDAdwany <AHMEDaladwan36@GMAIL.COM>
2024-04-08 19:46:26 -0700

    first commit

---
+++ src/main/java/pet/store/entity/Customer.java
@@ -0,0 +1,27 @@
+
+package pet.store.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+    
+    
+    private String customerName;
+    private String customerEmail;
+
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "customers", cascade = CascadeType.PERSIST)
+    private Set<PetStore> petStores = new HashSet<>();
+}
